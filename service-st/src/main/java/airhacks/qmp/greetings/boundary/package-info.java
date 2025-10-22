e502dc463e0c99946c696ced5d5234bbdec8f208
Adam Bien <abien@adam-bien.com>
2025-10-22 04:59:23 +0200

    client reusability mentioned

--- service-st/src/main/java/airhacks/qmp/greetings/boundary/package-info.java
+++ service-st/src/main/java/airhacks/qmp/greetings/boundary/package-info.java
@@ -4,5 +4,8 @@
  * The REST client interfaces reside in the same package as the resources under test to maintain
  * clear correspondence and simplify testing setup. This co-location makes it explicit which client
  * tests which resource and reduces navigation overhead during test development.
+ * <p>
+ * All MicroProfile REST client interfaces in this source directory (src/main/java) are packaged into a jar and can be
+ * reused as clients by other services.
  */
 package airhacks.qmp.greetings.boundary;
